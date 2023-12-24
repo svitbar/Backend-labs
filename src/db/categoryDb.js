094ccb5146b391e856d714392cf18ba6ad651a7c
svitbar <svitbar1102@gmail.com>
2023-12-24 09:22:48 +0200

    Remove database stubs

--- src/db/categoryDb.js
+++
@@ -1,9 +0,0 @@
-const Category = require('../entities/category');
-
-const categories = [
-  new Category(1, 'Drinks'),
-  new Category(2, 'Puzzles'),
-  new Category(3, 'Music'),
-];
-
-module.exports = categories;
